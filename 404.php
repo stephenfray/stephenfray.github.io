494ede14ebc7dab4e217f602e7b9193257f8abd3
stephenfray <stephen_hungry@hotmail.com>
2026-05-28 22:28:37 +0100

    inital commit

---
+++ 404.php
@@ -0,0 +1,53 @@
+<?php http_response_code(404); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stephen Fray</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap" rel="stylesheet">
+    <link href="/css/style.min.2.0.5.css" rel="stylesheet">
+    <link href="/css/404/404.min.css" rel="stylesheet">
+    <link rel="icon" href="/img/favicon.ico" type="image/x-icon" />
+    <meta name="robots" content="noindex">
+    
+</head>
+
+<body id="top">
+    <header>
+        <nav>
+            <div class="logo"><a href="/"><span>Step</span>hen Fray</a></div>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li>
+                    <a href="https://codepen.io/stepfray" target="_blank" rel="noreferrer" title="View my Codepen Profile"><img src="/img/nav/code-pen-icon.svg" alt="Codepen Icon" height="30" width="30" /></a>
+                    <a href="https://www.behance.net/stephen_fray" target="_blank" rel="noreferrer" title="View my Behance Profile"><img src="/img/nav/behance-icon.svg" alt="Behance Icon" height="30" width="30" /></a>
+                </li>
+            </ul>
+        </nav>
+
+    </header>
+    <section class="four-zero-four">
+
+        <div>
+            <img src="/img/cant-find.gif" alt="Can't find GIF">
+        </div>
+        <div>
+            <p>404 Page Not Found</p>
+            <h1>Whoops!</h1>
+            <h2>We couldn't find that page</h2>
+            <p><a href="/" title="Take me back to the homepage">Home</a><br/>Tel: 07907414580<br/>Email: me@stephenfray.co.uk</p>
+        </div>
+
+    </section>
+
+    
+
+</body>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,217 @@
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+//// Sticky Header
+///////////////////////////////////////
+
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+
+    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+        body.classList.add("scroll-down");
+    } else if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-down");
+    }
+    lastScroll = currentScroll < 0 ? 0 : currentScroll;
+});
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/// Replacing Anchor Links
+/////////////////////////////////////////////////
+// https://www.the-art-of-web.com/javascript/remove-anchor-links/
+
+document.querySelectorAll("header a").forEach(function(current) {
+    if (!current.hash) return;
+    if (current.origin + current.pathname != self.location.href) return;
+    (function(anchorPoint) {
+        if (anchorPoint) {
+            current.addEventListener("click", function(e) {
+                anchorPoint.scrollIntoView({ behavior: "smooth" });
+                e.preventDefault();
+            }, false);
+        }
+    })(document.querySelector(current.hash));
+});
+
+
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/// Read More Buttons
+/////////////////////////////////////////////////
+
+var readMoreButtons = document.querySelectorAll(".button.--read-more");
+
+for (var button of readMoreButtons) {
+    button.onclick = function(e) {
+
+        var currentButton = e.currentTarget;
+        var content = currentButton.closest('article').querySelector('.read-more-content');
+        var workDetails = currentButton.closest('article').querySelector('.work-details > div');
+
+        if (currentButton.classList.contains('active')) {
+            // CLOSE IT
+            // Actual show toggle
+            currentButton.textContent = 'Read More';
+            currentButton.classList.remove('active');
+            content.classList.remove('open');
+            // Remove fixed width
+            setTimeout(function() { workDetails.style.minHeight = 0; }, 500);
+
+        } else {
+            // OPEN IT
+            // Avoid layout shift by setting min height
+            workDetails.style.minHeight = `${workDetails.offsetHeight}px`;
+            // Actual show toggle
+            currentButton.textContent = 'Read Less';
+            currentButton.classList.add('active');
+            content.classList.add('open');
+        }
+
+
+    }
+}
+/// Animation triggers //////////////////////////////
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+//// Observer
+////////////////////////////////////////////////////////////////
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if(entry.isIntersecting) {
+        entry.target.classList.add('in-view');
+      }
+    });
+});
+
+const observerItem = document.querySelectorAll('.watch');
+observerItem.forEach((el) => observer.observe(el));
+
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+//// Nav Bar Highlight
+////////////////////////////////////////////////////////////////
+
+// Get The Basics
+const bufferDivider = 2; 
+const sectionHighLightBufferAmount = window.innerHeight / bufferDivider; 
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li");
+
+// Set an event listener to page scroll
+window.addEventListener("scroll", () => {
+  sections.forEach((section) => {
+    // How far we are from the top of the page
+    const scrollDistance = scrollY;
+
+    // How far the current section we're looping over is from the top
+    const sectionTop = section.offsetTop;
+
+    // Test if section is in view 
+    if (scrollDistance >= (sectionTop - sectionHighLightBufferAmount)) {
+
+      // Remove old active classes
+      navLinks.forEach(x => x.classList.remove("active"));
+
+      // Get new active id we can use to find the right nav li to highlight
+      const activeId = section.getAttribute("id");
+
+      // Loop find and add the active class to nav li
+      navLinks.forEach((li) => {
+        if (li.classList.contains(activeId))
+            li.classList.add("active");
+      });
+    }	
+  });
+});
+
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+//// Lightbox
+////////////////////////////////////////////////////////////////
+const _lightBoxImages = document.querySelectorAll(".lightbox-image");
+const _lightBoxResult = document.getElementById("lightbox-result");
+
+let ShowLightBox = (show) => {
+
+    console.log(show)
+
+	if (show) {
+		_lightBoxResult.classList.remove("fadeOut");
+		_lightBoxResult.classList.add("fadeIn");
+	} else {
+		_lightBoxResult.classList.remove("fadeIn");
+		_lightBoxResult.classList.add("fadeOut");
+	}
+};
+
+
+let BuildLightBox = (imageTitle, largeImageURL, imageAlt) => {
+	// Dynamically Add To Page
+	_lightBoxResult.innerHTML = LightBoxHTML(imageTitle, largeImageURL, imageAlt);
+	ShowLightBox(true);
+
+	// Attach Close Button Event Listener
+	let closeButtons = document.querySelectorAll(".close-lightbox");
+	closeButtons.forEach(function (button) {
+		button.addEventListener("click", function () {
+
+            console.log('clicked')
+
+			ShowLightBox(false);
+		});
+	});
+};
+
+let LightBoxHTML = (imageTitle, largeImageURL, imageAlt) => {
+    return `<div class="lightBox" id="lightBox">` +
+    `<div class="inner-wrap">` +
+    `<div class="content">` +
+    `<div class="close-lightbox-button close-lightbox"></div>` +
+    `<div class="image-box">` +
+    `<img src="${largeImageURL}" alt="${imageAlt}"/>` +
+    `</div>` +
+    `<div class="title">${imageTitle}</div>` +
+    `</div>` +
+    `</div>` +
+    `</div>` +
+    `</div>` +
+    `<div class="lightBox-background close-lightbox" id="lightBox-background"></div>`;
+};
+
+_lightBoxImages.forEach(function (image) {
+	image.addEventListener("click", function () {
+		let imageTitle = image.getAttribute("title"),
+			imageLargeURL = image.getAttribute("data-large-image-url"),
+			imageAlt = image.getAttribute("alt");
+
+		BuildLightBox(imageTitle, imageLargeURL, imageAlt);
+	});
+});
